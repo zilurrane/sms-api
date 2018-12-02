26e9d5f6ecc8e778e0814b54725150a808a44dab
Zilu <zilurrane@gmail.com>
2018-12-02 20:23:35 +0530

    Added TypeScript Node API Config

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/ping', async (_req, res) => {
+    res.send({
+        message : "pong"
+    })
+})
+
+app.listen(9000, () => console.log('listening on port 9000'))
